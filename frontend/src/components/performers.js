43b674d4f89291cda07592c5fff04fb03c0e77f9
Justin Pancho <jpancho@my.bcit.ca>
2019-04-08 14:57:58 -0700

    Test component GET performers

--- frontend/src/components/performers.js
+++ frontend/src/components/performers.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
 
-export default class Performers extends Component {
+const Performer = props => (
+  <tr>
+    <td>{props.performer.name}</td>
+    <td>{props.performer.style}</td>
+  </tr>
+);
+
+export default class Performers
+  extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {performers: []};
+  }
+
+  componentDidMount() {
+    let performerPath = 'http://localhost:3001/performers';
+    axios.get(performerPath)
+      .then(res => {
+        this.setState({performers: res.data});
+      })
+      .catch(function(err) {
+        console.log(err);
+      })
+  }
+
+  performerList() {
+    return this.state.performers.map(function(current, i) {
+      return <Performer performer={current} key={i} />;
+    });
+  }
+
   render() {
     return (
-      <h3>Performers component</h3>
+      <div>
+        <h3>Performers component</h3>
+        <table className="table table-striped" style={{ marginTop: 20}}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Style</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.performerList() }
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
