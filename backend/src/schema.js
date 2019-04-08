fc9769a86669f7c6597a147ffa7cf61429986a5e
jpancho <jpancho@sfu.ca>
2019-04-08 12:41:49 -0700

    add package

--- backend/src/schema.js
+++ backend/src/schema.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
+// "C:\Program Files\MongoDB\Server\4.0\bin\mongo.exe" mongo instance
+// "C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath="c:\data\db" mongod instance
+
 let Performer = new schema({
   name: String,
-  //style: String,
-  //price: String,
-  //description: String,
-  //booked: Boolean
+  style: String,
+  price: Number,
+  description: String,
+  booked: Boolean
 });
 
 module.exports = mongoose.model('Performer', Performer);
