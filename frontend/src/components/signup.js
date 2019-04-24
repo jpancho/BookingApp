b5dd57879dc3802fc248ba618d910e40ead0ba53
Justin Pancho <jpancho@my.bcit.ca>
2019-04-24 14:28:03 -0700

    Add textfields and button for signup

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -12,6 +12,21 @@ export default class Signup extends Component {
           <AppBar title="Enter User Details" />
           <TextField
             hintText="Enter Your Name"
+            label="Full name"
+          />
+          <TextField
+            id="outlined-email-input"
+            label="Username"
+            hintText="Enter Your Email"
+          />
+          <TextField
+            id="standard-password-input"
+            label="Password"
+            type="password"
+            hintText="Enter Your Password"
+          />
+          <RaisedButton
+            label="Sign up"
           />
         </React.Fragment>
       </MuiThemeProvider>
